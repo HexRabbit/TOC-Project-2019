cb02481122f252b0c34e392f59758e0bed29e1bc
HexRabbit <mikuqb8718@gmail.com>
2018-12-19 17:00:54 +0900

    Implement some bot function

--- app.py
+++ app.py
@@ -1,42 +1,16 @@
 from bottle import route, run, request, abort, static_file
+import requests
 
 from fsm import TocMachine
+import config
+import copy
+import random
+import traceback
+from utils import send_text_message, send_url_message, send_button_message
 
+userdata = {}
 
-VERIFY_TOKEN = "Your Webhook Verify Token"
-machine = TocMachine(
-    states=[
-        'user',
-        'state1',
-        'state2'
-    ],
-    transitions=[
-        {
-            'trigger': 'advance',
-            'source': 'user',
-            'dest': 'state1',
-            'conditions': 'is_going_to_state1'
-        },
-        {
-            'trigger': 'advance',
-            'source': 'user',
-            'dest': 'state2',
-            'conditions': 'is_going_to_state2'
-        },
-        {
-            'trigger': 'go_back',
-            'source': [
-                'state1',
-                'state2'
-            ],
-            'dest': 'user'
-        }
-    ],
-    initial='user',
-    auto_transitions=False,
-    show_conditions=True,
-)
-
+VERIFY_TOKEN = config.VERIFY_TOKEN
 
 @route("/webhook", method="GET")
 def setup_webhook():
@@ -55,21 +29,212 @@ def setup_webhook():
 @route("/webhook", method="POST")
 def webhook_handler():
     body = request.json
-    print('\nFSM STATE: ' + machine.state)
-    print('REQUEST BODY: ')
-    print(body)
 
-    if body['object'] == "page":
-        event = body['entry'][0]['messaging'][0]
-        machine.advance(event)
+    try:
+        if body['object'] == "page":
+            msg = ''
+            event = body['entry'][0]['messaging'][0]
+            id = int(event['sender']['id'])
+            if event.get('message'):
+                msg = event['message']['text']
+            elif event.get('postback'):
+                msg = event['postback']['payload']
+            state_handler(id, msg.strip())
+            return 'OK'
+        else:
+            return 'OK'
+    except:
         return 'OK'
 
 
-@route('/show-fsm', methods=['GET'])
-def show_fsm():
-    machine.get_graph().draw('fsm.png', prog='dot', format='png')
-    return static_file('fsm.png', root='./', mimetype='image/png')
+@route('/show-fsm/<id:int>', methods=['GET'])
+def show_fsm(id):
+    if userdata.get(id):
+        userdata[id]['machine'].get_graph().draw('./img/'+str(id)+'fsm.png', prog='dot', format='png')
+        return static_file(str(id)+'fsm.png', root='./img/', mimetype='image/png')
+    else:
+        abort(404)
+
+def state_handler(id, text):
+    if not userdata.get(id) or text == 'get_started':
+        userdata[id] = {
+            'state': 'new',
+            'record': 0,
+            'guess': 0,
+            'machine': TocMachine(
+                states=[
+                    'new',
+                    'menu',
+                    'game',
+                    'end',
+                    'info',
+                    'anime'
+                ],
+                transitions=[
+                    {
+                        'trigger': 'menu',
+                        'source': 'new',
+                        'dest': 'menu',
+                    },
+                    {
+                        'trigger': 'info',
+                        'source': 'new',
+                        'dest': 'info',
+                    },
+                    {
+                        'trigger': 'back',
+                        'source': 'info',
+                        'dest': 'new',
+                    },
+                    {
+                        'trigger': 'anime',
+                        'source': 'new',
+                        'dest': 'anime'
+                    },
+                    {
+                        'trigger': 'back',
+                        'source': 'anime',
+                        'dest': 'new'
+                    },
+                    {
+                        'trigger': 'game',
+                        'source': 'menu',
+                        'dest': 'game'
+                    },
+                    {
+                        'trigger': 'end',
+                        'source': 'game',
+                        'dest': 'end'
+                    },
+                    {
+                        'trigger': 'restart',
+                        'source': 'end',
+                        'dest': 'game'
+                    },
+                    {
+                        'trigger': 'new',
+                        'source': 'end',
+                        'dest': 'new'
+                    },
+                ],
+                initial='new',
+                auto_transitions=False,
+                show_conditions=True,
+            )
+        }
+        send_new_start(id)
+        return
+
+    print('User: {} STATE: {}'.format(id, userdata[id]['machine'].state))
+    state = userdata[id]['state']
+
+    if state == 'new':
+        if text == 'menu':
+            send_button_message(id, 'Guessing numbers', ['start'])
+            userdata[id]['state'] = 'menu'
+            userdata[id]['machine'].menu()
+        elif text == 'info':
+            send_button_message(id, 'This little application is create for TOC-project', ['back'])
+            userdata[id]['state'] = 'info'
+            userdata[id]['machine'].info()
+        elif text == 'anime':
+            anime_handler(id)
+        else:
+            send_text_message(id, 'Wrong input')
+
+    elif state == 'menu':
+        if text[:5] == 'start':
+            game_start(id)
+            userdata[id]['state'] = 'game'
+            userdata[id]['machine'].game()
+        else:
+            send_text_message(id, 'Wrong input')
+
+    elif state == 'game':
+        game_handler(id, text)
+
+    elif state == 'end':
+        if text == 'yes':
+            game_start(id)
+            userdata[id]['state'] = 'game'
+            userdata[id]['machine'].restart()
+        elif text == 'no':
+            send_new_start(id)
+            userdata[id]['state'] = 'new'
+            userdata[id]['machine'].new()
+        else:
+            send_text_message(id, 'Wrong input')
+
+    elif state == 'info':
+        if text == 'back':
+            send_new_start(id)
+            userdata[id]['state'] = 'new'
+            userdata[id]['machine'].back()
+        else:
+            send_text_message(id, 'Wrong input')
+    elif state == 'anime':
+        if text == 'back':
+            send_new_start(id)
+            userdata[id]['state'] = 'new'
+            userdata[id]['machine'].back()
+        else:
+            send_text_message(id, 'Wrong input')
+
+def send_new_start(id):
+    send_button_message(id, 'Use "menu" to view games, "info" to get info of this app, or "anime" to let RNGesus recommand you an anime!', ['menu', 'info', 'anime'])
+
+
+def game_start(id):
+    userdata[id]['guess'] = random.randint(0, 1000)
+    userdata[id]['count'] = 8
+    send_text_message(id, 'You have 8 times to guess a number (1-1000)')
+    send_text_message(id, 'GO!')
+
+def game_handler(id, text):
+    try:
+        userdata[id]['count'] -= 1
+        if int(text) == userdata[id]['guess']:
+            send_text_message(id, 'WOW! You guessed right!')
+            userdata[id]['state'] = 'end'
+            userdata[id]['machine'].end()
+        elif userdata[id]['count'] == 0:
+            send_text_message(id, 'GAMEOVER')
+            send_text_message(id, 'Answer: {}'.format(userdata[id]['guess']))
+            send_button_message(id, 'Retry?', ['yes', 'no'])
+            userdata[id]['state'] = 'end'
+            userdata[id]['machine'].end()
+        else:
+            if int(text) > userdata[id]['guess']:
+                send_text_message(id, 'Too big. Keep trying!')
+            else:
+                send_text_message(id, 'Too small. Keep trying!')
+            send_text_message(id, 'Remain {} times'.format(userdata[id]['count']))
+
+    except:
+        pass
+
+def anime_handler(id):
+    userdata[id]['state'] = 'anime'
+    userdata[id]['machine'].anime()
+    url = 'https://anidb.net/perl-bin/animedb.pl?show=anime&do.random=1'
+    headers = {
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+        'Accept-Language': 'zh-CN,zh;q=0.8,zh-TW;q=0.6',
+        'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Mobile Safari/537.36',
+        'Cache-Control': 'max-age=0',
+        'Connection': 'keep-alive'
+    }
+    try:
+        req = requests.get(url, headers=headers, allow_redirects=False)
+        resolved = 'https://anidb.net/perl-bin/' + req.headers['Location']
+        print(resolved)
+        send_text_message(id, 'Here\'s today recommendation:')
+        send_url_message(id, resolved)
+    except:
+        traceback.print_exc()
+        send_button_message(id, 'Some bad thing happened', ['back'])
 
 
 if __name__ == "__main__":
     run(host="localhost", port=5000, debug=True, reloader=True)
+
--- utils.py
+++ utils.py
@@ -1,8 +1,11 @@
 import requests
+import config
+from bs4 import BeautifulSoup
 
 
 GRAPH_URL = "https://graph.facebook.com/v2.6"
-ACCESS_TOKEN = "Your Page Access Token"
+ACCESS_TOKEN = config.ACCESS_TOKEN
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36'}
 
 
 def send_text_message(id, text):
@@ -16,6 +19,87 @@ def send_text_message(id, text):
     if response.status_code != 200:
         print("Unable to send message: " + response.text)
     return response
+
+def create_buttons(buttons):
+    l = []
+    for button in buttons:
+        l.append({ "type": "postback", "title": button, "payload": button })
+    return l
+
+def send_button_message(id, text, buttons):
+
+    url = "{0}/me/messages?access_token={1}".format(GRAPH_URL, ACCESS_TOKEN)
+    payload = {
+        "recipient":{
+            "id": id
+        },
+        "message":{
+            "attachment":{
+                "type": "template",
+                "payload":{
+                    "template_type": "button",
+                    "text": text,
+                    "buttons": create_buttons(buttons)
+                }
+            }
+        }
+    }
+    response = requests.post(url, json=payload)
+
+    if response.status_code != 200:
+        print("Unable to send message: " + response.text)
+
+    return response
+
+def send_url_message(id, target_url):
+    r = requests.get(target_url, headers = headers)
+    html = r.text
+    soup = BeautifulSoup(html, 'lxml')
+    img = soup.find('meta', attrs={'property': 'og:image'})['content']
+    title = soup.find('meta', attrs={'property': 'og:title'})['content']
+    description = soup.find('meta', attrs={'property': 'og:description'})['content']
+
+
+    url = "{0}/me/messages?access_token={1}".format(GRAPH_URL, ACCESS_TOKEN)
+    payload = {
+        "recipient":{
+            "id": id
+        },
+        "message":{
+            "attachment":{
+                "type":"template",
+                "payload":{
+                    "template_type":"generic",
+                    "elements":[
+                        {
+                            "title": title,
+                            "image_url": img,
+                            "subtitle": description,
+                            "default_action": {
+                                "type": "web_url",
+                                "url": target_url,
+                                "webview_height_ratio": "full",
+                            },
+                            "buttons":[
+                                {
+                                    "type": "postback",
+                                    "title": "back",
+                                    "payload": "back"
+                                }
+                            ]
+                        }
+                    ]
+                }
+            }
+        }
+    }
+    response = requests.post(url, json=payload)
+
+    if response.status_code != 200:
+        print("Unable to send message: " + response.text)
+
+    return response
+
 
 
 """
